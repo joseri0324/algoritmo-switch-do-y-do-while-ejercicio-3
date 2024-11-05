d3d25fa84e5aa672070086563492487bd8259316
jose <josesaa0324@gmail.com>
2024-11-04 21:51:21 -0500

    ingreso de formula y ejecucion

--- script.js
+++ script.js
@@ -2,3 +2,20 @@
 let x = 0
 let valor
 
+do {
+    valor = parseInt(prompt("Ingrese un valor (0000 para finalizar):"));
+    if (valor !== 0000) {
+        x += valor;
+    }
+} while (valor !== 0000);
+
+document.write("Valor es."+" "+ x +" ");
+
+if (x > 0) {
+    document.write("mayor que cero.");
+} else if (x < 0) {
+    document.write("menor que cero.");
+} else {
+    document.write("es cero.");
+}
+
